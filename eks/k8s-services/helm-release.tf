b0c756305e7f630b76ff4972824db6e8f6ca7f56
jasleen.notay <jasleen.notay@conasindia.com>
2026-09-08 00:26:13 +0530

    Adding helm-release & variables.tf

--- eks/k8s-services/helm-release.tf
+++ eks/k8s-services/helm-release.tf
@@ -5,3 +5,39 @@ data "aws_vpc" "eks_vpc_id" {
       values = [var.vpc_tag_value]
     }
 }
+
+# Create the helm_release
+resource "helm_release" "lbc_helm_release" {
+  name = "${var.project_name}-lbc-helm-release"
+  repository = "https://aws.github.io/eks-charts"
+  chart = "aws-load-balancer-controller"
+  version = "1.17.1"
+  cleanup_on_fail = true
+  description = "Helm releaase for LB deployment on EKS cluster"
+  namespace = "kube-system"
+  values = 
+  wait = true
+  set {
+    name = "clusterName"
+    value = var.eks_cluster_name
+  }
+
+  set {
+    name = "region"
+    value = var.aws_region
+  }
+
+  set {
+    name = "vpcId"
+    value = data.aws_vpc.eks_vpc_id.id
+  }
+
+
+}
+
+
+
+clusterName → var.eks_cluster_name
+vpcId → data.aws_vpc.eks_vpc_id.id
+region → probably a literal or a var
+serviceAccount.annotations["eks.amazonaws.com/role-arn"] → aws_iam_role.lbc_role.arn
--- eks/k8s-services/variables.tf
+++ eks/k8s-services/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+    description = "Enter the name of the AWS region for the infrastructure"
+    type = string
+    default = "us-east-1"  
+}
+
 variable "eks_cluster_name" {
     description = "Enter the name of the EKS cluster"
     type = string  
@@ -5,5 +11,10 @@ variable "eks_cluster_name" {
 
 variable "vpc_tag_value" {
     description = "Enter the value of the VPC tag name: ProjectName"
+    type = string  
+}
+
+variable "project_name" {
+    description = "Enter the name of the project relating to this deployment"
     type = string  
 }
